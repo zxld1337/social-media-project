ab49d78a2d0edd2e633d486471ecbd630a5dd40e
Silkó Levente <slevi1001@gmail.com>
2025-10-21 13:26:29 +0200

    Move .sln file to backend folder

--- backend/SocialMediaAPI/obj/Debug/net9.0/SocialMediaAPI.AssemblyInfo.cs
+++ backend/SocialMediaAPI/obj/Debug/net9.0/SocialMediaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d37585c9a847541697c7f212c090e84149435ed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
