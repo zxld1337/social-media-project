21b7bc6e1f439fd1f6c14229744633b515ab87de
BinoTwo <rohalyandor@gmail.com>
2025-11-17 16:34:00 +0100

    Empty Controllers

---
+++ backend/SocialMediaAPI/Controllers/CommentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaAPI.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
