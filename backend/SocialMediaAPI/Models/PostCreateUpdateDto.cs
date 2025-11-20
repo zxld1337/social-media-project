e8648afaf72e2a4ea184cd2c264515fdf6cf6ee1
BinoTwo <rohalyandor@gmail.com>
2025-11-20 14:07:03 +0100

    Added misssing 'using' for image handling. Created Models for posting.

---
+++ backend/SocialMediaAPI/Models/PostCreateUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http; //Needed for IFormFile!
+
+namespace SocialMediaAPI.Models
+{
+    public class PostCreateUpdateDto
+    {
+        public IFormFile? ImageFile { get; set; }
+        public string? Text { get; set; }
+    }
+}
