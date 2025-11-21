d12208f1afba05d1e99829ecbc4754739a0ad134
BinoTwo <rohalyandor@gmail.com>
2025-11-21 16:34:35 +0100

    Added Controller, Repository, Entity Model and DTOs for commenting

--- backend/SocialMediaAPI/Models/Entities/LikedPosts.cs
+++ backend/SocialMediaAPI/Models/Entities/LikedPosts.cs
@@ -2,7 +2,7 @@
 {
     public class LikedPosts
     {
-        public Guid Id { get; set; }
+        public required int Id { get; set; }
         public required int UserId { get; set; }
         public required int PostId { get; set; }
         public string? DateOfLike { get; set; }
