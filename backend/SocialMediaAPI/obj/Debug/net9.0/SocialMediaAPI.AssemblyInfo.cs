e718dd22eb73e99f8e7fcdf6cbc789bf3231d77e
BinoTwo <rohalyandor@gmail.com>
2025-12-01 08:10:40 +0100

    Added "follows" table into database

--- backend/SocialMediaAPI/obj/Debug/net9.0/SocialMediaAPI.AssemblyInfo.cs
+++ backend/SocialMediaAPI/obj/Debug/net9.0/SocialMediaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358e93e740177461defd8cd4779ac8fce2c1d409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1fab8b40a191970f91ff2b96341d54db77fbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
