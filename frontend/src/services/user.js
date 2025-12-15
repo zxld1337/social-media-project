2dfbfbd1802ee160eef64f00b62506d3ef4cbe49
Silkó Levente <slevi1001@gmail.com>
2025-12-15 14:56:09 +0100

    can get user data by id

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -1,12 +1,20 @@
 const BASE_URL = "http://localhost:5000/api/users";
 
 export const fetchUsers = async () => {
-    const response = await fetch(`${BASE_URL}`);
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${BASE_URL}`);
+  const data = await response.json();
+  return data;
 };
 
+export const fetchUserById = async (userId) => {
+  const response = await fetch(`${BASE_URL}/${userId}`, {
+    credentials: "include",
+  });
 
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
 
-
-
+  const data = await response.json();
+  return data;
+};
